2c8b74e4d4d04e82278b7793c372e29133e400b9
杨友永 <yangyouyong@hujiang.com>
2015-05-12 17:39:12 +0800

    C++ 面向对象

---
+++ CocosLearn01_CPP/CocosLearn01_CPP/Person.cpp
@@ -0,0 +1,13 @@
+//
+//  Person.cpp
+//  CocosLearn01_CPP
+//
+//  Created by yangyouyong on 15/5/12.
+//  Copyright (c) 2015年 yangyouyong. All rights reserved.
+//
+
+#include "Person.h"
+
+void Person::sayHello(){
+    printf("hello this is a C++ Proj");
+}
---
+++ CocosLearn01_CPP/CocosLearn01_CPP/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  CocosLearn01_CPP
+//
+//  Created by yangyouyong on 15/5/12.
+//  Copyright (c) 2015年 yangyouyong. All rights reserved.
+//
+
+#include <iostream>
+#include "Person.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    Person *p = new Person();
+    p->sayHello();
+    
+    
+    return 0;
+}
